dcc572c506c3550726fdab5bbdf8e3e7a1d82d1d
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2025-01-13 12:37:27 +0100

    Backend setup

---
+++ backend/src/models/Entry.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const entrySchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  content: String,
+  title: String,
+  mood: String,
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Entry", entrySchema);
