91dd3c1ffdabf96a94ea591bd4f698527c986317
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2025-01-24 11:00:45 +0100

    Add frontend structure

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "monolog app",
+  description:
+    "monolog is an app for keeping a personal diary and mood logging. Share you thoughts, pain, happiness and sadness with us",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
